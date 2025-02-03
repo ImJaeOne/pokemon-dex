72c236bbe2b6cd2a823f550afc9e23e4050a76d7
ImJaeOne <dlawi0108@naver.com>
2025-02-03 14:40:54 +0900

    update: PokemonCard Dashboard에서 재사용

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
-import pokemonBall from '../assets/pokeball-13iwdk7Y.png';
+import PokemonCard from './PokemonCard';
 
 const StDashboard = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding-bottom: 20px;
     margin-bottom: 20px;
+    border-radius: 10px;
+
+    background-color: #cdcdcd;
 `;
 
 const StDashBoardTitle = styled.h1``;
@@ -16,9 +20,7 @@ const StDashBoardList = styled.div`
     gap: 10px;
 `;
 
-const StDashBoardImg = styled.img`
-    width: 50px;
-`;
+
 
 const Dashboard = () => {
     const pokemonDummyList = Array.from({ length: 6 }, () => {
@@ -28,8 +30,8 @@ const Dashboard = () => {
         <StDashboard>
             <StDashBoardTitle>나의 포켓몬</StDashBoardTitle>
             <StDashBoardList>
-                {pokemonDummyList.map(() => {
-                    return <StDashBoardImg src={pokemonBall} />;
+                {pokemonDummyList.map((_, idx) => {
+                    return <PokemonCard key={idx}/>;
                 })}
             </StDashBoardList>
         </StDashboard>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import pokemonBall from '../assets/pokeball-13iwdk7Y.png';
+
 
 const StPokemonCard = styled.div`
     display: flex;
@@ -12,38 +14,47 @@ const StPokemonCard = styled.div`
     cursor: pointer;
     box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
     border-radius: 5px;
+    background-color: white;
 
-    &:hover{
+    &:hover {
         box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
         transform: translateY(-5px);
     }
 `;
 
+const StDashBoardImg = styled.img`
+    width: 50px;
+`;
+
 const StPokemonImg = styled.img`
     width: 100px;
     height: 100px;
-`
+`;
 const StPokemonName = styled.div`
     font-weight: bold;
-`
+`;
 
 const StPokemonId = styled.div`
     color: #bbbbbb;
-`
+`;
 
 const StPokemonBtn = styled.button`
     border-radius: 10px;
     border: none;
-    padding: 5px 10px; 
-`
+    padding: 5px 10px;
+`;
 
-const PokemonCard = ({ item }) => {
+const PokemonCard = ({ item = {} }) => {
     return (
         <StPokemonCard>
-            <StPokemonImg src={item.img_url} />
-            <StPokemonName>{item.korean_name}</StPokemonName>
-            <StPokemonId>No. {String(item.id).padStart(3, '0')}</StPokemonId>
-            <StPokemonBtn>등록</StPokemonBtn>
+            {Object.keys(item).length === 0 ? <StDashBoardImg src={pokemonBall}/> : (
+                <>
+                    <StPokemonImg src={item.img_url} />
+                    <StPokemonName>{item.korean_name}</StPokemonName>
+                    <StPokemonId>No. {String(item.id).padStart(3, '0')}</StPokemonId>
+                    <StPokemonBtn>등록</StPokemonBtn>
+                </>
+            )}
         </StPokemonCard>
     );
 };
