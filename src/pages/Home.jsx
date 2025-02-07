2612f800ece621bbd8cad549b2dec32ac4213203
ImJaeOne <dlawi0108@naver.com>
2025-02-07 16:37:40 +0900

    update: navigate > Link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link  } from 'react-router-dom';
 import styled from 'styled-components';
 import pokemonLogo from '../assets/pokemon-logo-RN0wntMB.png';
 
@@ -15,21 +15,21 @@ const StHomeLogo = styled.img`
     width: 50%;
 `;
 
-const StDexBtn = styled.button`
+const StDexBtn = styled(Link)`
     border: none;
     border-radius: 5px;
     color: white;
     padding: 20px;
     background-color: red;
     cursor: pointer;
+    text-decoration: none;
 `;
 
 const Home = () => {
-    const navigate = useNavigate();
     return (
         <StHomeContainer>
             <StHomeLogo src={pokemonLogo} />
-            <StDexBtn onClick={() => navigate('/dex')}>포켓몬 도감 시작하기</StDexBtn>
+            <StDexBtn to='/dex'>포켓몬 도감 시작하기</StDexBtn>
         </StHomeContainer>
     );
 };
